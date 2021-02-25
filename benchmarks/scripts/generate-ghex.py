761a9cb1095c593a1e15606bd78bace7bd7bc820
John Biddiscombe <biddisco@cscs.ch>
2021-02-25 21:19:49 +0100

    Script generation options/etc

--- benchmarks/scripts/generate-ghex.py
+++ benchmarks/scripts/generate-ghex.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[98]:
+# In[10]:
 
 
 from itertools import product
 import math
 import numpy as np
 import inspect
-import os.path
+import os
 import time
 import subprocess
 from IPython.display import Image, display, HTML
 import importlib
 import socket
-#
+import argparse
+
+# working dir
+cwd = os.getcwd()
+
+# hostname + cleanup login node 'daint101' etc
 hostname = socket.gethostname()
 if hostname.startswith('daint'):
     hostname = 'daint'
+
+# name of this script
 scriptname = inspect.getframeinfo(inspect.currentframe()).filename
 scriptpath = os.path.dirname(os.path.abspath(scriptname))
-print(f'Scriptpath {scriptpath} \nHostname {hostname}')
+
+# summary
+print(f'CWD        : {cwd} \nScriptpath : {scriptpath} \nHostname   : {hostname}')
 
 
-# In[99]:
-
-
-# this string will be substituted by cmake 
-bin_dir = "@BIN_DIR@"
-run_dir = "@RUN_DIR@"
-
-
-# In[103]:
+# In[11]:
 
 
 def is_notebook():
@@ -45,7 +46,7 @@ def is_notebook():
             return False  # Other type (?)
     except NameError:
         return False      # Probably standard Python interpreter
-    
+
 if is_notebook():
     # this makes the notebook wider on a larger screen using %x of the display
     display(HTML("<style>.container { width:100% !important; }</style>"))
@@ -53,7 +54,42 @@ if is_notebook():
     get_ipython().system('jupyter nbconvert --to script generate-ghex.ipynb')
 
 
-# In[72]:
+# In[12]:
+
+
+# ------------------------------------------------------------------
+# Command line params
+# ------------------------------------------------------------------
+def get_command_line_args(notebook_args=None):
+    parser = argparse.ArgumentParser(description='Generator for ghex benchmarks')
+    parser.add_argument('-d', '--dir', default=cwd, action='store', help='base directory to generate job scripts in')
+    if is_notebook():
+        parser.add_argument('-f', help='seems to be defaulted by jupyter')
+        return parser.parse_args(notebook_args)
+    return parser.parse_args()
+
+notebook_args = '--dir /home/biddisco/benchmarking-results/test'.split()
+if is_notebook():
+    args = get_command_line_args(notebook_args)
+else:
+    args = get_command_line_args()
+
+
+# In[13]:
+
+
+# strings with @xxx@ will be substituted by cmake
+binary_dir = "@BIN_DIR@"
+
+if args.dir:
+    run_dir = args.dir
+else:
+    run_dir = "@RUN_DIR@"
+
+print(f'Generating scripts in {run_dir}')
+
+
+# In[14]:
 
 
 #
@@ -68,7 +104,7 @@ def normalized_sigmoid_fkt(center, width, x):
    output between 0 and 1
    '''
    s = 1/(1+np.exp(width*(x-center)))
-   return s 
+   return s
    #return 1*(s-min(s))/(max(s)-min(s)) # normalize function to 0-1
 
 def num_messages(vmax, vmin, center, width, x):
@@ -76,16 +112,17 @@ def num_messages(vmax, vmin, center, width, x):
     return vmin + (vmax-vmin)*s #(s-vmin)/(vmax-vmin) # normalize function to 0-1
 
 
-# In[73]:
+# In[15]:
 
 
 cscs = {}
 
+# jb laptop
 cscs["oryx2"] = {
   "Cores": 8,
   "Threads per core": 2,
   "Allowed rpns": [1, 2],
-  "thread_array": [1,2,4,8],    
+  "thread_array": [1,2,4,8],
   "sleeptime":0,
   "Run command": "mpiexec -n {total_ranks} --oversubscribe",
   "Batch preamble": """
@@ -98,7 +135,7 @@ export OMP_NUM_THREADS={threads}
 """
 }
 
-# mc nodes config
+# daint mc nodes config
 cscs["daint"] = {
   "Cores": 36,
   "Threads per core": 2,
@@ -131,15 +168,15 @@ printenv > env.txt
 }
 
 
-# In[74]:
+# In[16]:
 
 
 #
 # Generate Job script preamble
 #
 def init_job_text(system, run_name, time_min, nodes, threads, inflight, size):
-    return system["Batch preamble"].format(run_name=run_name, 
-                                           time_min=time_min, 
+    return system["Batch preamble"].format(run_name=run_name,
+                                           time_min=time_min,
                                            nodes=nodes,
                                            size=size,
                                            inflight=inflight,
@@ -158,7 +195,7 @@ def run_command(system, total_ranks, cpus_per_rank):
     return system["Run command"].format(total_ranks=total_ranks, cpus_per_rank=cpus_per_rank, threads_per_rank=threads_per_rank)
 
 #
-# create dir + write final script for sbatch/shell or other job launcher 
+# create dir + write final script for sbatch/shell or other job launcher
 #
 def write_job_file(system, launch_file, job_dir, job_text, suffix=''):
     job_path = os.path.expanduser(job_dir)
@@ -181,60 +218,61 @@ def ghex(system, bin_dir, timeout, transport, progs, nodes, threads, msg, size, 
     # mpi oes not have suffix, other transport layers use '_libfabric', '_ucx', eetc
     suffix = f'_{transport}' if transport!='mpi' else ''
     for prog in progs:
-        
+
         # generate the program commmand with command line params
         cmd = f"{bin_dir}/{prog}{suffix} {msg} {size} {inflight}"
-        
+
         # get the launch command (mpiexec, srun, etc)
         run_cmd = run_command(system, total_ranks, threads)
-        
+
         # simple version of benchmark
         temp = "\n" + f"{env} timeout {timeout} {run_cmd} {cmd} >> {prog}_{msg}_{size}_{inflight}.out".strip()
         whole_cmd += temp +'\n'
-        
+
         # for libfabric, run benchmark again with extra environment options to control execution
         if transport=='libfabric':
             lf_env = env + 'LIBFABRIC_AUTO_PROGRESS=1'
             temp = "\n" + f"{lf_env} timeout {timeout} {run_cmd} {cmd} >> {prog}_{msg}_{size}_{inflight}.out".strip()
             whole_cmd += temp +'\n'
-            
-            lf_env = env + 'LIBFABRIC_ENDPOINT_MULTI=1'
+
+            lf_env = env + 'LIBFABRIC_ENDPOINT_TYPE=multiple'
             temp = "\n" + f"{lf_env} timeout {timeout} {run_cmd} {cmd} >> {prog}_{msg}_{size}_{inflight}.out".strip()
             whole_cmd += temp +'\n'
-            
-            lf_env = env + 'LIBFABRIC_ENDPOINT_THREADLOCAL=1'
+
+            lf_env = env + 'LIBFABRIC_ENDPOINT_TYPE=threadlocal'
             temp = "\n" + f"{lf_env} timeout {timeout} {run_cmd} {cmd} >> {prog}_{msg}_{size}_{inflight}.out".strip()
             whole_cmd += temp +'\n'
-            
-            lf_env = env + 'LIBFABRIC_AUTO_PROGRESS=1 ' + 'LIBFABRIC_ENDPOINT_MULTI=1'
+
+            lf_env = env + 'LIBFABRIC_AUTO_PROGRESS=1 ' + 'LIBFABRIC_ENDPOINT_TYPE=multiple'
             temp = "\n" + f"{lf_env} timeout {timeout} {run_cmd} {cmd} >> {prog}_{msg}_{size}_{inflight}.out".strip()
             whole_cmd += temp +'\n'
-            
-            lf_env = env + 'LIBFABRIC_AUTO_PROGRESS=1 ' + 'LIBFABRIC_ENDPOINT_THREADLOCAL=1'
+
+            lf_env = env + 'LIBFABRIC_AUTO_PROGRESS=1 ' + 'LIBFABRIC_ENDPOINT_TYPE=threadlocal'
             temp = "\n" + f"{lf_env} timeout {timeout} {run_cmd} {cmd} >> {prog}_{msg}_{size}_{inflight}.out".strip()
             whole_cmd += temp +'\n'
-            
+
     return whole_cmd
 
 
-# In[75]:
+# In[17]:
 
 
 system = cscs[hostname]
 #
 job_name       = 'ghex-benchmark'
-timeout        = 180        # seconds per benchmark
-time_min       = timeout*20 # total time estimate  
+timeout        = 400        # seconds per benchmark
+time_min       = timeout*20 # total time estimate
 timestr        = time.strftime('%H:%M:%S', time.gmtime(time_min))
 ranks_per_node = 1
 nodes_arr = [2]
 trans_arr = ['libfabric', 'mpi']
 thrd_arr  = system['thread_array']
-size_arr  = [1,1000,10000,100000,500000,1000000]
-nmsg_lut  = {1:500000, 
-             1000:500000, 
+size_arr  = [1,100,1000,10000,100000,500000,1000000]
+nmsg_lut  = {1:500000,
+             100:500000,
+             1000:500000,
              5000:250000,
-             10000:250000, 
+             10000:250000,
              50000:250000,
              100000:250000,
              200000:250000,
@@ -245,40 +283,41 @@ nmsg_lut  = {1:500000,
 #for i in size_arr:
 #    print(int(num_messages(1E6, 25E3, 1E5, 1E-5, i)))
 
-flight_arr= [1,2,4,8,16,32,64]
+flight_arr= [1,4,64]
 prog_arr  = ["ghex_p2p_bi_cb_avail_mt", "ghex_p2p_bi_cb_wait_mt", "ghex_p2p_bi_ft_avail_mt", "ghex_p2p_bi_ft_wait_mt"]
 
 
-# In[76]:
+# In[18]:
 
 
 combos = 0
 
-job_launch = f"{run_dir}/launch.sh"
-job_launch_file = open(job_launch, "w")
+if run_dir.startswith('@'):
+    print(f'Skipping creation of job launch file for {run_dir}')
+else:
+    job_launch = f"{run_dir}/launch.sh"
+    job_launch_file = open(job_launch, "w")
 
 # generate all combinations in one monster loop
 for nodes, transport, threads, size, inflight in product(
-    nodes_arr, trans_arr, thrd_arr, size_arr, flight_arr):        
-        
-    combos += 1 
-    
-    extra_flags = ""    
+    nodes_arr, trans_arr, thrd_arr, size_arr, flight_arr):
+
+    extra_flags = ""
     suffix = ""
     # number of messages (niter)
     msg = int(num_messages(1E6, 25E3, 1E5, 1E-5, size))
     msg = nmsg_lut[size]
     # create the output directory for each job
     job_dir = make_job_directory(run_dir, 'ghex', transport, nodes, threads, inflight, size)
-    
+
     # first part of boiler plate job script
-    job_text = init_job_text(system, job_name, timestr, nodes, threads, size, msg)    
-    
+    job_text = init_job_text(system, job_name, timestr, nodes, threads, size, msg)
+
     # application specific part of job script
     job_text += ghex(
         system,
-        bin_dir,
-        timeout, 
+        binary_dir,
+        timeout,
         transport,
         prog_arr,
         nodes,
@@ -289,10 +328,15 @@ for nodes, transport, threads, size, inflight in product(
         suffix,
         extra_flags,
     )
-    
-    # uncomment this run jobs
-    write_job_file(system, job_launch_file, job_dir, job_text)
+    # debugging
     # print(job_dir, '\n', job_text, '\n\n\n\n')
+
+    combos += 1
+
+    if combos==1:
+        print('Uncommment the following line to perform the job creation')
+    # uncomment this to create job scripts
+    write_job_file(system, job_launch_file, job_dir, job_text)
 
 print(combos)
 
